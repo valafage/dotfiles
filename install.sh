29c4145f8c07ba1d4d86c544b749b3c808d4ccb3
Valentin Lafage <vlafage.dev@free.fr>
2024-12-02 16:14:52 +0100

    feat: add install script :hammer:
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# bash
+
+ln -s $PWD/bash/.bashrc ~/.bashrc
+ln -s $PWD/bash/.bash_aliases ~/.bash_aliases
+ln -s $PWD/bash/.bash_variables ~/.bash_variables
+
+# editor
+
+ln -s $PWD/editor/.vimrc ~/.vimrc
+
+# scripts
+
+sudo ln -s $PWD/scripts/system/remove_old_logs.sh /usr/local/bin/rm-logs
+
+# terminal
+
+ln -s $PWD/terminal/.tmux.conf ~/.tmux.conf 
+

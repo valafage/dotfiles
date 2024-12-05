effe09210a6746bdf039e488e8901bf89440b427
Valentin Lafage <vlafage.dev@free.fr>
2024-12-05 16:45:58 +0100

    feat: ask before replacing the 'scripts' directory
    two options available:
      - overwrite content
      - merge new content into existing folder (default)
    
    resolves #4
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>
    (cherry picked from commit 6d5ffdc2fae7d758724b7e8ca7de5cd20cd359a5)

--- install.sh
+++ install.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+source "./scripts/install.sh"
+
+set -e
+
 # bash
 
-ln -sf $PWD/bash/.bashrc ~/.bashrc
-ln -sf $PWD/bash/.bash_aliases ~/.bash_aliases
-ln -sf $PWD/bash/.bash_variables ~/.bash_variables
+ln -sf "$PWD/bash/.bashrc" "$HOME/.bashrc"
+ln -sf "$PWD/bash/.bash_aliases" "$HOME/.bash_aliases"
+ln -sf "$PWD/bash/.bash_variables" "$HOME/.bash_variables"
 
-source ~/.bashrc
+source "$HOME/.bashrc"
 
 # editor
 
-ln -sf $PWD/editor/.vimrc ~/.vimrc
+ln -sf "$PWD/editor/.vimrc" "$HOME/.vimrc"
 
 # scripts
 
-ln -sf $PWD/scripts ~/scripts
-
-sudo ln -sf ~/scripts/system/remove_old_logs.sh /usr/local/bin/rm-logs
+install_scripts
 
 # terminal
 
-ln -sf $PWD/terminal/.tmux.conf ~/.tmux.conf 
+ln -sf "$PWD/terminal/.tmux.conf" "$HOME/.tmux.conf" 
 
 # vcs
 
-ln -sf $PWD/vcs/.gitconfig ~/.gitconfig
+ln -sf "$PWD/vcs/.gitconfig" "$HOME/.gitconfig"
 

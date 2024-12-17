141ff25e3c7ec07b9b0b1e8973601c236b3fba01
Valentin Lafage <vlafage.dev@free.fr>
2024-12-17 10:32:26 +0100

    chore: add code linter and pre-commit hook :art:
    resolves #8
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+directory = Path(__file__).resolve().parent
+with open(directory / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="dotfiles",
+    version="1.0.0",
+    author="Valentin Lafage",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "linting": [
+            "black",
+            "flake8",
+            "pre-commit",
+        ],
+    },
+    python_requires=">=3.9",
+)

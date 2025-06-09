e335debd6c5dd5f0808b3e9a9ccec66fc54bf47e
Valentin Lafage <vlafage.dev@free.fr>
2025-06-09 11:25:56 +0200

    feat: add ssh
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

--- install.sh
+++ install.sh
@@ -13,8 +13,23 @@ set_up_git() {
   create_symlink "$PWD/git/.gitconfig"
 }
 
+set_up_ssh() {
+  local dst="$HOME/.ssh"
+  mkdir -p "$dst"
+  create_symlink "$PWD/ssh/config" "$dst"
+  set_up_ssh_agent
+}
+
+set_up_ssh_agent() {
+  local dst="$HOME/.config/1Password/ssh"
+  if [[ -d "$dst" ]]; then
+    create_symlink "$PWD/ssh/agent.toml" "$dst"
+  fi
+}
+
 main() {
   set_up_git
+  set_up_ssh
 }
 
 main
